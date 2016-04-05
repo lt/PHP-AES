9e37d31a5ca093c749472776d8b8cf7f9d3dd387
Leigh <leight@gmail.com>
2016-04-05 19:25:37 +0100

    NIST CFB tests

---
+++ test/CFB/CFB128GFSbox128Test.php
@@ -0,0 +1,49 @@
+<?php
+
+# CAVS 11.1
+# Config info for aes_values
+# AESVS GFSbox test data for CFB128
+# State : Encrypt and Decrypt
+# Key Length : 128
+# Generated on Fri Apr 22 15:11:53 2011
+
+namespace AES\Test;
+
+use AES\CFB;
+use AES\Key;
+
+class CFB128GFSbox128 extends \PHPUnit_Framework_TestCase
+{
+    function caseProvider()
+    {
+        return [
+            ['00000000000000000000000000000000', 'f34481ec3cc627bacd5dc3fb08f273e6', '00000000000000000000000000000000', '0336763e966d92595a567cc9ce537f5e'],
+            ['00000000000000000000000000000000', '9798c4640bad75c7c3227db910174e72', '00000000000000000000000000000000', 'a9a1631bf4996954ebc093957b234589'],
+            ['00000000000000000000000000000000', '96ab5c2ff612d9dfaae8c31f30c42168', '00000000000000000000000000000000', 'ff4f8391a6a40ca5b25d23bedd44a597'],
+            ['00000000000000000000000000000000', '6a118a874519e64e9963798a503f1d35', '00000000000000000000000000000000', 'dc43be40be0e53712f7e2bf5ca707209'],
+            ['00000000000000000000000000000000', 'cb9fceec81286ca3e989bd979b0cb284', '00000000000000000000000000000000', '92beedab1895a94faa69b632e5cc47ce'],
+            ['00000000000000000000000000000000', 'b26aeb1874e47ca8358ff22378f09144', '00000000000000000000000000000000', '459264f4798f6a78bacb89c15ed3d601'],
+            ['00000000000000000000000000000000', '58c8e00b2631686d54eab84b91f0aca1', '00000000000000000000000000000000', '08a4e2efec8a8e3312ca7460b9040bbf']
+        ];
+    }
+
+    /**
+     * @dataProvider caseProvider
+     */
+    function testEncrypt($key, $iv, $plaintext, $ciphertext)
+    {
+        $cfb = new CFB(new Key(hex2bin($key)), hex2bin($iv));
+        $result = $cfb->encrypt(hex2bin($plaintext));
+        $this->assertSame(hex2bin($ciphertext), $result);
+    }
+
+    /**
+     * @dataProvider caseProvider
+     */
+    function testDecrypt($key, $iv, $plaintext, $ciphertext)
+    {
+        $cfb = new CFB(new Key(hex2bin($key)), hex2bin($iv));
+        $result = $cfb->decrypt(hex2bin($ciphertext));
+        $this->assertSame(hex2bin($plaintext), $result);
+    }
+}
---
+++ test/CFB/CFB128GFSbox192Test.php
@@ -0,0 +1,48 @@
+<?php
+
+# CAVS 11.1
+# Config info for aes_values
+# AESVS GFSbox test data for CFB128
+# State : Encrypt and Decrypt
+# Key Length : 192
+# Generated on Fri Apr 22 15:11:55 2011
+
+namespace AES\Test;
+
+use AES\CFB;
+use AES\Key;
+
+class CFB128GFSbox192 extends \PHPUnit_Framework_TestCase
+{
+    function caseProvider()
+    {
+        return [
+            ['000000000000000000000000000000000000000000000000', '1b077a6af4b7f98229de786d7516b639', '00000000000000000000000000000000', '275cfc0413d8ccb70513c3859b1d0f72'],
+            ['000000000000000000000000000000000000000000000000', '9c2d8842e5f48f57648205d39a239af1', '00000000000000000000000000000000', 'c9b8135ff1b5adc413dfd053b21bd96d'],
+            ['000000000000000000000000000000000000000000000000', 'bff52510095f518ecca60af4205444bb', '00000000000000000000000000000000', '4a3650c3371ce2eb35e389a171427440'],
+            ['000000000000000000000000000000000000000000000000', '51719783d3185a535bd75adc65071ce1', '00000000000000000000000000000000', '4f354592ff7c8847d2d0870ca9481b7c'],
+            ['000000000000000000000000000000000000000000000000', '26aa49dcfe7629a8901a69a9914e6dfd', '00000000000000000000000000000000', 'd5e08bf9a182e857cf40b3a36ee248cc'],
+            ['000000000000000000000000000000000000000000000000', '941a4773058224e1ef66d10e0a6ee782', '00000000000000000000000000000000', '067cd9d3749207791841562507fa9626']
+        ];
+    }
+
+    /**
+     * @dataProvider caseProvider
+     */
+    function testEncrypt($key, $iv, $plaintext, $ciphertext)
+    {
+        $cfb = new CFB(new Key(hex2bin($key)), hex2bin($iv));
+        $result = $cfb->encrypt(hex2bin($plaintext));
+        $this->assertSame(hex2bin($ciphertext), $result);
+    }
+
+    /**
+     * @dataProvider caseProvider
+     */
+    function testDecrypt($key, $iv, $plaintext, $ciphertext)
+    {
+        $cfb = new CFB(new Key(hex2bin($key)), hex2bin($iv));
+        $result = $cfb->decrypt(hex2bin($ciphertext));
+        $this->assertSame(hex2bin($plaintext), $result);
+    }
+}
